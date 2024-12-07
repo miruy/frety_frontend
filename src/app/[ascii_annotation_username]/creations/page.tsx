b81b58f373463141e8bf4cd838dfa2ea233efd2c
miruy <dbflarla4966@naver.com>
2024-12-07 13:19:58 +0900

    update

--- src/app/[ascii_annotation_username]/creations/page.tsx
+++ src/app/[ascii_annotation_username]/creations/page.tsx
@@ -1,22 +1,46 @@
+// import NotFound from "@/app/not-found";
+// import {GetServerSidePropsContext} from "next";
+// import MyCreatedAllTabs from "@/components/page/MyCreatedAllTabs";
+//
+// const MyCreatedTabsPage = async (context: GetServerSidePropsContext) => {
+//
+//     const {ascii_annotation_username} = context.params!;
+//
+//     if (typeof ascii_annotation_username !== 'string') {
+//         return <NotFound/>;
+//     }
+//
+//     const decodingUserName = decodeURIComponent(ascii_annotation_username); // @dbflarla4966
+//     const userName = decodingUserName.replace("@", "") // dbflarla4966
+//
+//     try {
+//         return (
+//             <MyCreatedAllTabs userName={userName}/>
+//         );
+//     } catch {
+//         return <NotFound/>;
+//     }
+// };
+//
+// export default MyCreatedTabsPage;
+
 import NotFound from "@/app/not-found";
-import {GetServerSidePropsContext} from "next";
 import MyCreatedAllTabs from "@/components/page/MyCreatedAllTabs";
 
-const MyCreatedTabsPage = async (context: GetServerSidePropsContext) => {
+interface Props {
+    params: {
+        ascii_annotation_username: string;
+    };
+}
 
-    const {ascii_annotation_username} = context.params!;
-
-    if (typeof ascii_annotation_username !== 'string') {
-        return <NotFound/>;
-    }
+const MyCreatedTabsPage = async ({params}: Props) => {
+    const {ascii_annotation_username} = params;
 
     const decodingUserName = decodeURIComponent(ascii_annotation_username); // @dbflarla4966
-    const userName = decodingUserName.replace("@", "") // dbflarla4966
+    const userName = decodingUserName.replace("@", ""); // dbflarla4966
 
     try {
-        return (
-            <MyCreatedAllTabs userName={userName}/>
-        );
+        return <MyCreatedAllTabs userName={userName}/>;
     } catch {
         return <NotFound/>;
     }
