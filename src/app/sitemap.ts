978805c6187c1c99c16a2c3b01d653e6bd575b19
miruy <dbflarla4966@naver.com>
2024-12-11 21:42:06 +0900

    update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -30,41 +30,36 @@ const defaultSiteMap: MetadataRoute.Sitemap = [
 ];
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    try {
+        let allTabs: SearchTabsResponse[] = [];
+        let currentPage = 0;
+        const pageSize = 10;
 
-    let allTabs: SearchTabsResponse[] = [];
-    let currentPage = 0;
-    const pageSize = 10;
+        while (true) {
+            const tabsResponse = await searchTabs({
+                sort: "RECENT",
+                page: currentPage,
+                pageSize: pageSize,
+            });
 
-    // 페이지네이션을 고려하여 모든 탭을 가져옴
-    while (true) {
-        // `searchTabs` API 호출, page와 pageSize 파라미터 사용
-        const tabsResponse = await searchTabs({
-            sort: "RECENT",
-            page: currentPage,
-            pageSize: pageSize
-        });
+            if (!tabsResponse.data || tabsResponse.data.length === 0) {
+                break;
+            }
 
-        // 받아온 데이터가 없다면 종료
-        if (!tabsResponse.data || tabsResponse.data.length === 0) {
-            break;
+            allTabs = [...allTabs, ...tabsResponse.data];
+            currentPage++;
         }
 
-        // 받아온 데이터를 allTabs 배열에 추가
-        allTabs = [...allTabs, ...tabsResponse.data];
-
-        // 다음 페이지로 이동
-        currentPage++;
-    }
-
-    // `tabs.data`가 undefined일 경우 빈 배열을 반환
-    const sitemapFromTabs: MetadataRoute.Sitemap = allTabs.map((tab) => {
-        return {
-            url: 'https://www.frety.me/tab/' + tab.id, // 실제 URL 경로에 맞게 수정
-            lastModified: new Date(tab.createdAt!),  // 실제 날짜 정보 사용
+        const sitemapFromTabs: MetadataRoute.Sitemap = allTabs.map((tab) => ({
+            url: `https://www.frety.me/tab/${tab.id}`,
+            lastModified: new Date(tab.createdAt!),
             changeFrequency: "daily",
             priority: 0.7,
-        };
-    }) || [];  // undefined일 경우 빈 배열로 처리
+        }));
 
-    return [...defaultSiteMap, ...sitemapFromTabs];
+        return [...defaultSiteMap, ...sitemapFromTabs];
+    } catch (error) {
+        console.error("Error generating sitemap:", error);
+        return defaultSiteMap; // 기본 사이트맵만 반환
+    }
 }
